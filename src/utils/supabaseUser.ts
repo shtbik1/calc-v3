bc753e2e9dd85ac644359ca0a8dd554c7327639a
shtbik1 <ishtykov0@gmail.com>
2025-04-06 13:48:18 +0300

    revert to diplom

--- src/utils/supabaseUser.ts
+++ src/utils/supabaseUser.ts
@@ -5,8 +5,3 @@ import { DB_ANON_KEY, DB_SERV_KEY, DB_URL } from "./constants"
 export const supabase = createClient(DB_URL!, DB_ANON_KEY!)
 
 export const supabaseServ = createClient(DB_URL!, DB_SERV_KEY!)
-
-// const res = await supabase.auth.signInWithPassword({
-//   email: process.env.NEXT_SUPABASE_DB_MAIL || "",
-//   password: process.env.NEXT_SUPABASE_DB_PASS || "",
-// })
